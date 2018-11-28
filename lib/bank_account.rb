a19d79a39965129320f4f9f0cd8189162ef15a63
Linda Chang <lindachang186@gmail.com>
2018-11-28 22:14:04 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,22 @@
+require "pry"
+
 class BankAccount
+  attr_accessor :bank_account, :balance, :status
+  attr_reader :name
+
+    def initialize(name)
+      @name = name
+      @bank_account = bank_account
+      @status = "open"
+      @balance = 1000
+    end
+
+    def deposit(amount)
+      @balance = @balance + amount
+    end
+
+    def display_balance
+      "Your balance is $#{@balance}." 
+    end
 
 end
